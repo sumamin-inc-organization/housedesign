6bebb27ab5dbfb92f8f276a587e2943213faf97e
m2357aaa <m2357kinu8810@yahoo.co.jp>
2024-07-24 17:58:27 +0900

    headerメニュー作成、hamburgerメニュー作成

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>House Design</title>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/common.js" defer></script>
+
+        <!-- slider -->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+        <?php wp_head(); ?>
+    </head>
