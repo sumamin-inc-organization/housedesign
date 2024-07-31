20e86866df2250726708813b1e888018e5da22cb
m2357aaa <m2357kinu8810@yahoo.co.jp>
2024-07-31 15:25:58 +0900

    TOPページ作成

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,11 @@
 // hamburger
 jQuery(document).ready(function($) {
-    $('#js-hamburger-menu, .navigation__link').on('click', function () {
-        if ($('.navigation').hasClass('is-open')) {
-            closeNavigation();
+    $('.header_top #js-hamburger-menu, .header_fixed #js-hamburger-menu, .navigation__link').on('click', function () {
+        var header = $(this).closest('header');
+        if (header.find('.navigation').hasClass('is-open')) {
+            closeNavigation(header);
         } else {
-            openNavigation();
+            openNavigation(header);
         }
     });
     $(document).on('click', function (event) {
@@ -13,15 +14,19 @@ jQuery(document).ready(function($) {
             closeNavigation();
         }
     });
-    function openNavigation() {
-        $('.navigation').css('right', '-100%').css('display', 'block').animate({ right: '0' }, 500, function () {
+    function openNavigation(header) {
+        header.find('.navigation').css('right', '-100%').css('display', 'block').animate({ right: '0' }, 500, function () {
             $(this).addClass('is-open');
+            if (header.hasClass('header_top')) {
+                $('body').addClass('no-scroll'); // header_topの場合、スクロールを無効にする
+            }
         });
-        $('.hamburger-menu').addClass('hamburger-menu--open');
+        header.find('.hamburger-menu').addClass('hamburger-menu--open');
     }
     function closeNavigation() {
         $('.navigation').animate({ right: '-100%' }, 500, function () {
             $(this).removeClass('is-open').css('display', 'none');
+            $('body').removeClass('no-scroll'); // スクロールを有効に戻す
         });
         $('.hamburger-menu').removeClass('hamburger-menu--open');
     }
@@ -52,4 +57,37 @@ window.addEventListener('scroll', function() {
     } else {
         headerFixed.classList.remove('show');
     }
+});
+
+function beforeAfterSlider() {
+    document.getElementById("js-boxBefore").style.width = document.getElementById("js-sliderRange").value + "%";
+}
+
+$('.scroller_inner').slick({
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 1500,
+    slidesToShow: 5.2,
+    slidesToScroll: 1,
+    cssEase: "linear",
+    pauseOnHover: false,
+    draggable: false,
+    swipe: false,
+    touchMove: false,
+    responsive: [
+        {
+            breakpoint: 1050,
+            settings: {
+                speed: 2000,
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                speed: 2000,
+                slidesToShow: 2.2,
+            }
+        },
+    ],
 });
