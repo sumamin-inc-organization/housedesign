3e10dc215e99b0c53cfea9ca2d2714c74127f0a8
EimiMatsuzaki <150405009+EimiMatsuzaki@users.noreply.github.com>
2024-08-01 13:14:08 +0900

    フッター修正

--- front-page.php
+++ front-page.php
@@ -1,20 +1,5 @@
-<?php get_header();?>
-
-<?php get_template_part('header_nav_top'); ?>
-
+<?php get_header(); ?>
 <main>
-    <section class="kv">
-        <div class="kv_content">
-            <video class="kv_movie" width="100vw" height="100vh" autoplay muted playsinline loop>
-                <source src="<?php echo get_template_directory_uri(); ?>/assets/images/top/kv.mp4" type="video/mp4">
-                お使いのブラウザは動画タグをサポートしていません。
-            </video>
-            <video class="kv_movie_sp" width="100vw" height="100vh" autoplay muted playsinline loop>
-                <source src="<?php echo get_template_directory_uri(); ?>/assets/images/top/sp/kv.mp4" type="video/mp4">
-                お使いのブラウザは動画タグをサポートしていません。
-            </video>
-        </div>
-    </section>
-</main>
 
+</main>
 <?php get_footer(); ?>
